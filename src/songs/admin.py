60604fd58ad6562cea99b2842990c9c3e4281325
Bartosz Kazuła <bartosz@kazula.eu>
2022-12-19 00:58:54 +0100

    [MOM-8] Added a possibility to sort songs with drag&drop in admin panel

--- src/songs/admin.py
+++ src/songs/admin.py
@@ -1,11 +1,12 @@
+from adminsortable2.admin import SortableAdminMixin
 from django.contrib import admin
 from django.utils.html import mark_safe
 from .models import Song
 
 
 @admin.register(Song)
-class SongAdmin(admin.ModelAdmin):
-    list_display = ("title", "original_title", "order", "full_lyrics")
+class SongAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ("order", "title", "original_title", "full_lyrics")
 
     def full_lyrics(self, obj: Song):
         return mark_safe(obj.rendered_lyrics)
